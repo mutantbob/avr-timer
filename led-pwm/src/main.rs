3b3f06456d31e4d49f36ca8f450bea553ec10347
Robert Forsman <git@thoth.purplefrog.com>
2022-05-19 14:28:16 -0400

    switch to using 10-bit PWM

--- led-pwm/src/main.rs
+++ led-pwm/src/main.rs
@@ -1,20 +1,23 @@
 #![no_std]
 #![no_main]
 
-/// Connect a current-limiting resistor and LED in series between
-/// OC1A (= PB1 = pin 9 on the Uno) and ground.
-/// This app will control the LED brightness using the PWM output of Timer 1A.
-///
+//! Connect a current-limiting resistor and LED in series between
+//! OC1A (= PB1 = pin 9 on the Uno) and ground.
+//! This app will control the LED brightness using the PWM output of Timer 1A.
+//!
+//! I created this example with the hope that it can be incorporated as an example in
+//! https://github.com/Rahix/avr-hal/tree/main/examples/arduino-uno
+//! and this example is therefore licensed under whatever terms are necessary to accomplish this.
+//! - thoth950@gmail.com
+
 use crate::timer_common::{debug_dump, WaveformGenerationMode};
 use arduino_hal::default_serial;
-use arduino_hal::pac::tc0::tccr0a::COM0A_A;
-use arduino_hal::pac::tc0::tccr0a::WGM0_A::PWM_PHASE;
-use arduino_hal::pac::tc0::tccr0b::CS0_A;
+use arduino_hal::hal::port::{PB1, PB2};
 use arduino_hal::pac::tc1::tccr1a::COM1A_A;
 use arduino_hal::pac::tc1::tccr1b::CS1_A;
-use arduino_hal::pac::{TC0, TC1};
+use arduino_hal::pac::TC1;
 use arduino_hal::port::Pin;
-use avr_hal_generic::port::mode::Io;
+use avr_hal_generic::port::mode::Output;
 use avr_hal_generic::port::PinOps;
 use avr_hal_generic::void;
 use panic_halt as _;
@@ -33,110 +36,49 @@ fn main() -> ! {
 
     let _ = uwriteln!(&mut serial, "hello from Arduino");
 
-    match 1 {
-        1 => pulse_width_modulation_1a(pins.d9, pins.d10, pins.d13, dp.TC1, &mut serial),
-        99 => {
-            let mut led = pins.d9.into_output();
-            led.set_high();
-            loop {
-                avr_device::asm::sleep()
-            }
-        }
-        _ => pulse_width_modulation_0a(dp.TC0, pins.d6, pins.d5, pins.d13, &mut serial),
-    }
+    pulse_width_modulation_1a(
+        pins.d9.into_output(),
+        pins.d10.into_output(),
+        pins.d13.into_output(),
+        dp.TC1,
+        &mut serial,
+    )
 }
 
-pub fn pulse_width_modulation_0a<
-    I: Io,
-    P1: PinOps,
-    P2: PinOps,
-    P3: PinOps,
-    W: uWrite<Error = void::Void>,
->(
-    timer0: TC0,
-    d6: Pin<I, P1>,
-    d5: Pin<I, P2>,
-    led: Pin<I, P3>,
-    serial: &mut W,
-) -> ! {
-    let _oc0a = d6.into_output();
-
-    let _oc0b = d5.into_output();
-
-    // PWM: phase correct
-    timer0.tccr0a.write(|w| w.wgm0().variant(PWM_PHASE));
-    timer0.tccr0b.write(|w| w.wgm02().bit(false));
-
-    timer0
-        .tccr0a
-        .write(|w| w.com0a().variant(COM0A_A::MATCH_CLEAR));
-
-    timer0
-        .tccr0b
-        .write(|w| w.foc0a().bit(false).cs0().variant(CS0_A::PRESCALE_256));
-
-    let mut led = led.into_output();
-
-    let _ = uwriteln!(serial, "timer0;");
-
-    let mut duty_cycle = 0x80;
-    loop {
-        timer0.ocr0a.write(|w| unsafe { w.bits(duty_cycle) });
-
-        arduino_hal::delay_ms(500);
-
-        led.toggle();
-
-        duty_cycle += 0x40;
-    }
-}
-
-pub fn pulse_width_modulation_1a<
-    I: Io,
-    P1: PinOps,
-    P2: PinOps,
-    P3: PinOps,
-    W: uWrite<Error = void::Void>,
->(
-    d9: Pin<I, P1>,
-    d10: Pin<I, P2>,
-    led: Pin<I, P3>,
+pub fn pulse_width_modulation_1a<P3: PinOps, W: uWrite<Error = void::Void>>(
+    _oc1a: Pin<Output, PB1>, // OC1A is pin 9 on an Uno
+    _oc1b: Pin<Output, PB2>, // OC1B is pin 10 on an Uno
+    led: Pin<Output, P3>,
     timer1: TC1,
     serial: &mut W,
 ) -> ! {
-    let _oc1a = d9.into_output();
-
-    let _oc1b = d10.into_output();
+    const TIMER_MAX: u16 = 0x3ff; // 10-bit max
 
     timer_common::rig_timer_1a(
         &timer1,
         serial,
         COM1A_A::MATCH_CLEAR,
-        WaveformGenerationMode::PWMPhaseCorrectICR1,
+        WaveformGenerationMode::PWMPhaseCorrect10Bit,
         CS1_A::DIRECT,
     )
     .void_unwrap();
-
-    timer1.icr1.write(|w| unsafe { w.bits(0xffff) });
 
     // timer1.timsk1.write(|w| w.toie1().set_bit()); // since we haven't defined an interrupt handler, this triggers a crash
 
     let mut led = led.into_output();
 
-    let states: [u16; 4] = [0x2000, 0x4000, 0x8000, 0xffff]; // duty cycle: gets shifted up to a 16-bit value
-    let mut iter = states.iter().cycle();
-
-    let _ = debug_dump(serial, &timer1);
+    const PATTERN: [u16; 4] = [TIMER_MAX / 8, TIMER_MAX / 4, TIMER_MAX / 2, TIMER_MAX];
+    let mut iter = PATTERN.iter().cycle();
 
     loop {
         let duty_cycle = *iter.next().unwrap();
         timer1.ocr1a.write(|w| unsafe { w.bits(duty_cycle) });
-        timer1.ocr1b.write(|w| unsafe { w.bits(duty_cycle >> 1) });
+        let _ = uwriteln!(serial, "duty cycle {}/{}", duty_cycle, TIMER_MAX);
 
         arduino_hal::delay_ms(500);
 
-        let n = timer1.tcnt1.read().bits();
-        let _ = uwriteln!(serial, "tcnt={}", n);
+        /* let n = timer1.tcnt1.read().bits();
+        let _ = uwriteln!(serial, "tcnt={}", n); */
 
         led.toggle();
     }
